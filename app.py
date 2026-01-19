25abd48f96124d172b5a4309e339fa7d0ccc90d0
YuvarajSYali-developer <syaliyuvaraj@gmail.com>
2026-01-20 00:57:26 +0530

    Bump version to V4.1 to force deploy

--- app.py
+++ app.py
@@ -156,5 +156,6 @@ def predict():
         return jsonify({'status': 'error', 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    print("[+] System V4.1 Loaded... Starting Server...")
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=True)
